a46a7e9e1a00943ba94391b8eef544a009ef1ec2
Scott47 <scottsilver47@gmail.com>
2021-08-17 07:56:57 -0500

    feat: dropdown for selecting cat and image for header

--- src/scripts/CatsRateDogs.js
+++ src/scripts/CatsRateDogs.js
@@ -3,7 +3,7 @@ import { CatSelect } from "./Cats.js"
 export const CatsRateDogs = () => {
     return `
     <h1>Cats Rate Dogs</h1>
-    <div id="cats-rate-dogs-image"><img alt="Cats Rate Dogs" src="images/CatsRateDogs1.png" /></div>
+    <div id="header-image"><img alt="Cats Rate Dogs" src="images/CatsRateDogs1.png"></div>
     <section id='cat-reviewers'>
     ${CatSelect()}
     </section>
